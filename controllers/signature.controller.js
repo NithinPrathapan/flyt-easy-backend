4d0e0088c720789f53e42493fa53e3e62d332bdc
nithinprathapan <nithinprathapan32567@gmail.com>
2025-06-16 22:26:11 +0530

    signature controller

--- controllers/signature.controller.js
+++ controllers/signature.controller.js
@@ -1,32 +1,49 @@
-import { fetchSignatureToken } from "../utils/generateToken.js";
-
 export const generateSignature = async (req, res) => {
-  const requiredFields = [
-    "MerchantID",
-    "ApiKey",
-    "ClientID",
-    "Password",
-    "AgentCode",
-    "BrowserKey",
-  ];
-
-  // Check for missing fields
-  const missingFields = requiredFields.filter((field) => !(field in req.body));
-  if (missingFields.length > 0) {
-    return res.status(400).json({
-      message: "Missing required fields",
-      missingFields,
-    });
-  }
-
   try {
-    // Pass the fields from the request body to the token generator
-    const tokenResponse = await fetchSignatureToken(req.body);
-    res.status(200).json(tokenResponse);
-  } catch (err) {
+    const { MerchantID, ApiKey, ClientID, Password, AgentCode, BrowserKey } =
+      req.body;
+
+    if (
+      !MerchantID ||
+      !ApiKey ||
+      !ClientID ||
+      !Password ||
+      !AgentCode ||
+      !BrowserKey
+    ) {
+      return res.status(400).json({
+        message: "Missing required fields",
+      });
+    }
+
+    const requiredFields = [
+      "MerchantID",
+      "ApiKey",
+      "ClientID",
+      "Password",
+      "AgentCode",
+      "BrowserKey",
+    ];
+
+    const missingFields = requiredFields.filter(
+      (field) => !(field in req.body)
+    );
+
+    if (missingFields.length > 0) {
+      return res.status(400).json({
+        message: "Missing required fields",
+        missingFields,
+      });
+    }
+
+    const signature = await fetchSignatureToken(req.body);
+
+    res.status(200).json(signature);
+  } catch (error) {
+    console.log(error);
     res.status(500).json({
-      message: "Failed to fetch signature token",
-      error: err.message,
+      message: "Failed to generate signature",
+      error: error.message,
     });
   }
 };
