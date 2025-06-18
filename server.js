9282dc83cd297f5ab8f8b48353d6c9f171a6ff78
nithinprathapan <nithinprathapan32567@gmail.com>
2025-06-18 16:37:06 +0530

    ui added flight

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import indexRoutes from "./routes/index.routes.js";
-
+import cors from "cors";
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static("public"));
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api", indexRoutes);
 
 app.get("/", (req, res) => {
