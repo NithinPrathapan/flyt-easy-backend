84a462804c7a66a2ccdf908172dece9d7e28b3f7
nithinprathapan <nithinprathapan32567@gmail.com>
2025-06-23 13:19:58 +0530

    port changed

--- server.js
+++ server.js
@@ -22,14 +22,11 @@ app.use(
     credentials: true,
   })
 );
-// set the login form first to show when entering the app instead of the index.html
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "login.html"));
-// });
 app.use("/api", indexRoutes);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
